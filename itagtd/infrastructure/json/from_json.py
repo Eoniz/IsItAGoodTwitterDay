1c06b1f4a5b956aab00ad56a6c888705f88bcd2a
Eoniz <nathan.artisien@gmail.com>
2020-07-21 23:49:36 +0200

    Cleaning up from_json function

--- itagtd/infrastructure/json/from_json.py
+++ itagtd/infrastructure/json/from_json.py
@@ -10,20 +10,21 @@ def from_json(data, cls):
         for value in data:
             instance.append(from_json(value, list_type))
         return instance
-    elif issubclass(cls, Dict):
-            key_type = cls.__args__[0]
-            val_type = cls.__args__[1]
-            instance: dict = dict()
-            for key, value in data.items():
-                instance.update(from_json(key, key_type), from_json(value, val_type))
-            return instance
-    else:
-        instance : cls = cls()
-        for name, value in data.items():
-            field_type = annotations.get(name)
-            if inspect.isclass(field_type) and isinstance(value, (dict, tuple, list, set, frozenset)):
-                setattr(instance, name, from_json(value, field_type))
-            else:
-                setattr(instance, name, value)
 
+    if issubclass(cls, Dict):
+        key_type = cls.__args__[0]
+        val_type = cls.__args__[1]
+        instance: dict = dict()
+        for key, value in data.items():
+            instance.update(from_json(key, key_type), from_json(value, val_type))
         return instance
+
+    instance: cls = cls()
+    for name, value in data.items():
+        field_type = annotations.get(name)
+        if inspect.isclass(field_type) and isinstance(value, (dict, tuple, list, set, frozenset)):
+            setattr(instance, name, from_json(value, field_type))
+        else:
+            setattr(instance, name, value)
+
+    return instance
